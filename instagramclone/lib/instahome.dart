51a18329dd407c5caae68b38de29b6106867c7a0
suryaprakashs <imssurya@outlook.com>
2020-11-29 13:25:11 +0530

    initial commit

--- instagramclone/lib/instahome.dart
+++ instagramclone/lib/instahome.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:instagramclone/instabody.dart';
 
 class InstaHome extends StatelessWidget {
   final topBar = AppBar(
@@ -13,7 +14,7 @@ class InstaHome extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: topBar,
-      body: Container(),
+      body: InstaBody(),
       bottomNavigationBar: Container(
         color: Colors.white,
         height: 50.0,
--- instagramclone/lib/instalist.dart
+++ instagramclone/lib/instalist.dart
@@ -9,8 +9,60 @@ class InstaList extends StatelessWidget {
       itemBuilder: (context, index) => index == 0
           ? SizedBox(
               child: InstaStories(),
+              height: 100.0,
             )
-          : Column(),
+          : Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.fromLTRB(16.0, 16.0, 8.0, 16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          Container(
+                            height: 40.0,
+                            width: 40.0,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              image: DecorationImage(
+                                fit: BoxFit.fill,
+                                image: NetworkImage("url"),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 10.0,
+                          ),
+                          Text("imssurya"),
+                        ],
+                      ),
+                      IconButton(icon: Icon(Icons.more_vert), onPressed: () {}),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Icon(Icons.favorite),
+                      SizedBox(
+                        width: 16.0,
+                      ),
+                      Icon(Icons.comment),
+                      SizedBox(
+                        width: 16.0,
+                      ),
+                      Icon(Icons.place),
+                    ],
+                  ),
+                )
+              ],
+            ),
     );
   }
 }
